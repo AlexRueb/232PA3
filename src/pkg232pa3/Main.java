aac091da2e095f996ca40cb989358ba6be64b23f
AlexRueb <alexrueb668@gmail.com>
2018-04-06 13:54:18 -0600

    works on any square matrix

--- src/pkg232pa3/Main.java
+++ src/pkg232pa3/Main.java
@@ -11,9 +11,10 @@ import java.lang.*;
 import java.io.*;
 
 public class Main {
-    private static final int[][] matrix = new int[5][5];
+    private static int[][] matrix;
     private static final String inputFile = "input/input.txt";
-
+    public static int edges;
+    public static int vertices;
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
             Scanner sc = new Scanner(new File(inputFile));
@@ -21,9 +22,14 @@ public class Main {
             while (sc.hasNext()) {
                 String s = sc.nextLine();
                 String[] thisOne = s.split(",");
-                int[] thisTwo = new int[5];
-                for (int i = 0; i<5; i++){
+                vertices = thisOne.length;
+                if(matrix == null){
+                    matrix = new int[vertices][vertices];
+                }
+                int[] thisTwo = new int[vertices];
+                for (int i = 0; i<vertices; i++){
                 thisTwo[i] = Integer.parseInt(thisOne[i]);
+                if(thisTwo[i] != 0){ edges++;}
                 }
                 matrix[matrixPos] = thisTwo;
                 matrixPos++;
@@ -43,15 +49,21 @@ public class Main {
 
     //Kruskals algorithm - Alex Rueb
     public static void kruskal() {
-        
+        Edge allEdges[] = new Edge[Main.edges];
+        int edgePos = 0;
         for (int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
-                if(matrix[i][j] == 0){
-                    
-                } 
+                if(matrix[i][j] == 0){} 
+                else {
+                    Edge edge = new Edge();
+                    edge.weight = matrix[i][j];
+                    edge.source = i;
+                    edge.dest = j;
+                    allEdges[edgePos] = edge;
+                }
             }
         }
-
+        System.out.println("");
     }
 
     //Floyd-Warshall's algorithm - Nicolas Tonjum
